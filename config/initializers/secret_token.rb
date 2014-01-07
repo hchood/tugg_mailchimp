ce0cf3c3c9c4a8ab8037b995a080a59d4a4df2af
Helen Hood <helen.c.hood@gmail.com>
2014-01-07 15:33:22 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TuggMailchimp::Application.config.secret_key_base = 'd214f5ffbc1e82458c2316c098ff0fb021debd3e0a10566bb4442982d274d2d20507e683185ef9bbb914f0fa83656453306dc8328cb1937fb929180cce2e4142'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TuggMailchimp::Application.config.session_store :cookie_store, key: '_tugg_mailchimp_session'
